ad3274fa57876573ca68e39c07c572d03c284157
Joel Wanjohi <joelwanjohi207@gmail.com>
2024-09-24 12:11:56 +0300

    my first commit

---
+++ lib/Widgets/custom_button.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:therapy_ai/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  final Color? color;
+  const CustomButton(
+      {super.key,
+      required this.text,
+      required this.onPressed,
+      this.color = buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onPressed(),
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+}
